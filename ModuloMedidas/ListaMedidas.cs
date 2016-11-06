c130521bdd2ed911aa21e8b654865f9aacb6ee8a
Pgsuarez <pgsuarez@esei.uvigo.es>
2016-11-06 21:53:48 +0100

    Modulo de medidas inicial

---
+++ ModuloMedidas/ListaMedidas.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace ModuloMedidas
+{
+	public class ListaMedidas
+	{
+		private List<Medidas> listaMedidas=new List<Medidas>();
+
+		public ListaMedidas ()
+		{
+		}
+
+		public List<Medidas> getListaMedidas(){
+			return this.listaMedidas;
+		}
+
+		public void setListaMedidas(List<Medidas> lm){
+			this.listaMedidas = lm;
+		}
+
+		public void Add(Medidas m){
+			this.listaMedidas.Add (m);
+		}
+
+		public void Guardar(){
+			var doc = new XmlTextWriter( "listamedidas.xml", Encoding.UTF8 );
+			doc.WriteStartDocument();
+
+			doc.WriteStartElement( "listamedidas" );
+
+			foreach(var m in this.listaMedidas) {
+				doc.WriteStartElement( "medidas" );
+
+				doc.WriteStartElement( "fecha" );
+				doc.WriteString( m.Fecha.ToString() );
+				doc.WriteEndElement();
+
+				doc.WriteStartElement( "peso" );
+				doc.WriteString( m.Peso.ToString() );
+				doc.WriteEndElement();
+
+				doc.WriteStartElement( "circunferenciaabdominal" );
+				doc.WriteString( m.CircunferenciaAbdominal.ToString() );
+				doc.WriteEndElement();
+
+				doc.WriteEndElement();
+			}
+
+			doc.WriteEndElement();
+
+			doc.WriteEndDocument();
+			doc.Close();
+		}
+
+		public void Recuperar(){
+			var docXml = new XmlDocument( );
+
+			docXml.Load ("listamedidas.xml");
+			foreach (XmlNode nodo in docXml.DocumentElement.ChildNodes) {
+				if (nodo.Name == "medidas") {
+					StringBuilder p=new StringBuilder();
+					StringBuilder ca=new StringBuilder();
+					StringBuilder f=new StringBuilder();
+					foreach (XmlNode child in nodo.ChildNodes) {
+						if (child.Name == "peso") {
+							p.Append( child.InnerText);
+						} else if (child.Name == "circunferenciaabdominal") {
+							ca.Append(child.InnerText);
+						} else if (child.Name == "fecha") {
+							f.Append(child.InnerText);
+						}
+					}
+					this.Add (new Medidas (float.Parse (p.ToString ()), float.Parse (ca.ToString ()), DateTime.Parse (f.ToString ())));
+				}
+			}
+		}
+
+		public override string ToString ()
+		{
+			StringBuilder toret = new StringBuilder();
+
+			foreach (Medidas m in listaMedidas) {
+				toret.Append (m.ToString ());
+			}
+			return toret.ToString();
+		}
+	}
+}
+
---
+++ ModuloMedidas/Medidas.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ModuloMedidas
+{
+	public class Medidas
+	{
+
+		public Medidas(float p,float ca, DateTime f){
+			this.Peso = p;
+			this.CircunferenciaAbdominal = ca;
+			this.Fecha = f;
+		}
+
+		public DateTime Fecha {
+			get;
+			set;
+		}
+
+		public float Peso {
+			get;
+			set;
+		}
+
+		public float CircunferenciaAbdominal {
+			get;
+			set;
+		}
+
+		public void Guardar(){
+			var med = new XElement ("medidas",
+				          new XElement ("peso", Peso),
+				          new XElement ("circunferenciaabdominal", CircunferenciaAbdominal),
+				          new XElement ("fecha", Fecha.ToString ()));
+			med.Save ("medidas.xml");
+		}
+
+		public void Recuperar(){
+			XElement med = XElement.Load ("medidas.xml");
+			this.Peso = float.Parse (med.Element ("medidas").Element ("peso").ToString());
+			this.CircunferenciaAbdominal = float.Parse (med.Element ("medidas").Element ("circunferenciaabdominal").ToString());
+			this.Fecha = DateTime.Parse (med.Element ("medidas").Element ("fecha").ToString());
+		}
+
+		public override string ToString ()
+		{
+			return string.Format ("[Medidas: Fecha={0}, Peso={1}, CircunferenciaAbdominal={2}]\n", Fecha, Peso, CircunferenciaAbdominal);
+		}
+	}
+
+}
+
